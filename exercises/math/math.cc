eebd33fcf4eda9af902b80a6fadf64fc6b2a1558
Sebastian Dahl Negendahl <sebbevb94@gmail.com>
2025-02-04 18:06:11 +0100

    Added Stirling approximation to math.cc

--- exercises/math/math.cc
+++ exercises/math/math.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include "sfuns.h"
 int main() {
     double sqrt2 = std::sqrt(2);
     double fifthroot2 = std::pow(2, 0.2);
@@ -9,6 +10,9 @@ int main() {
     std::cout << "2^(1/5) = " << fifthroot2 << std::endl;
     std::cout << "e^pi = " << e_pi << std::endl;
     std::cout << "pi^e = " << pi_e << std::endl;
+    for (int i =1; i<=10; i++){
+        std::cout << "gamma(" << i << ") = " << sfuns::fgamma(i) << std::endl;
+    }
     return 0;
 }
 
