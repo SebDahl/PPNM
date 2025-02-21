1b855657b0ad70ec19a090efcd841358331bc6e4
Sebastian Dahl Negendahl <sebbevb94@gmail.com>
2025-02-21 08:19:04 +0100

    Started (barely) on plotting

--- exercises/math/sfuns.h
+++ exercises/math/sfuns.h
@@ -5,6 +5,8 @@
 
 namespace sfuns {
     double fgamma(double x);  // Declaration only
+    double erf(double x);  // Declaration only
+    double erfc(double x);  // Declaration only
 }
 
 #endif // SFUNS_H
