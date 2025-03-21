57f5ea743255571285ce3a4f4c77acb2ff9b821d
Sebastian Dahl Negendahl <sebbevb94@gmail.com>
2025-03-21 15:59:15 +0100

    Work on splines

--- homework/splines/main.cpp
+++ homework/splines/main.cpp
@@ -8,15 +8,16 @@
 #include <functional>
 #include <string>
 #include <cmath>
+#include <stdexcept>
 
 
 #include <vector>
 
 struct qspline {
-    std::vector<double> x, y, b, c; // Use std::vector<double>
+    pp::vector x, y, b, c;
 
     // Constructor
-    qspline(const std::vector<double>& xs, const std::vector<double>& ys)
+    qspline(const pp::vector& xs, const pp::vector& ys)
         : x(xs), y(ys), b(xs.size()), c(xs.size()) 
     {
         // Code to compute b and c
@@ -28,14 +29,63 @@ struct qspline {
         return 0.0; // Placeholder
     }
 };
+int binsearch(pp::vector x, double z)
+	{/* locates the interval for z by bisection */ 
+	if( z<x[0] || z>x[x.size()-1] ){ throw std::runtime_error("binsearch: bad z");}
+	int i=0, j=x.size()-1;
+	while(j-i>1){
+		int mid=(i+j)/2;
+		if(z>x[mid]) i=mid; else j=mid;
+		}
+	return i;
+	}
+double linterp(pp::vector x, pp::vector y, double z){
+    int i=binsearch(x,z);
+    double dx=x[i+1]-x[i]; if(!(dx>0)){ throw std::runtime_error("uups...");}
+    double dy=y[i+1]-y[i];
+    return y[i]+dy/dx*(z-x[i]);
+    }
+
+
+
+double linterpIntegrate(pp::vector x, pp::vector y, double z){
+
+}
 
 
 
 
 int main(){
 
-    double asdasd =1;
-    pp::vector asdasd(3);
+    pp::vector x_i(10);
+    pp::vector y_i(10);
+    for (int i = 0; i < 10; i++) {
+        x_i[i] = i;
+        y_i[i] = std::cos(x_i[i]);
+    }
+
+    pp::vector::write(x_i, "x_i.txt");
+    pp::vector::write(y_i, "y_i.txt");
+
+    double z = 3.5;
+    double y = linterp(x_i, y_i, z);
+    std::cout << "Interpolated value at z = " << z << " is " << y << std::endl;
+    pp::vector x_interpolated(0);
+    pp::vector x_integrated(0);
+    pp::vector y_interpolated(0);
+    pp::vector y_integrated(0);
+    std::cout << "Test" << x_i[9] << std::endl;
+
+    for(double i = 0; i < 9; i += 0.1){
+        x_interpolated.append(i);
+        x_integrated.append(i);
+        y_interpolated.append(linterp(x_i, y_i, i));
+        y_integrated.append(linterpIntegrate(x_i, y_i, i));
+    }
+    pp::vector::write(x_interpolated, "x_interpolated.txt");
+    pp::vector::write(y_interpolated, "y_interpolated.txt");
+    pp::vector::write(x_integrated, "x_integrated.txt");
+    pp::vector::write(y_integrated, "y_integrated.txt");
     
 
     return 0;
